e4eaee1442b0cfa69b0e5b099ead25b6a3a7a09f
DOROSHYK <76391172+DOROSHYK@users.noreply.github.com>
2021-07-24 20:39:46 +0000

    Deploying to gh-pages from @ DOROSHYK/team-project-02-group-11@a1b9bf2ec6f0f651ff8f89bccc79076685d5acb8 🚀

--- src.2bea1882.js
+++ src.2bea1882.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-02-group-11/src.03278b8a.js.map
+//# sourceMappingURL=/team-project-02-group-11/src.2bea1882.js.map
