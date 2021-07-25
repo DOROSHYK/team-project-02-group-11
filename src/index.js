578c107447fff753fb8ae63b4c639bbbc469afa6
Doroshuk <dorosukolga100@gmail.com>
2021-07-25 22:07:52 +0300

    підправила частину відкриття зактиття модалки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './sass/main.scss';
+import './js/close-modal.js';
--- src/js/close-modal.js
+++ src/js/close-modal.js
@@ -1,15 +1,25 @@
+const refs = {
+    closeBtn: document.querySelector('[data-modal-close]'),
+  modalWindow: document.querySelector('.backdrop'),
+  link: document.querySelector('.footer_link'),
+}
 
+refs.link.addEventListener('click', onCloseBtn)
 
-// refs.closeBtn.addEventListener('click', onCloseBtn);
+refs.closeBtn.addEventListener('click', onCloseBtn);
 export function onCloseBtn(event) {
-    refs.modalWindow.classList.remove('is-open');
-    refs.closeBtn.remuveEventListener('click', onCloseBtn);
+    refs.modalWindow.classList.remove('is-hidden');
+    // refs.closeBtn.removeEventListener('click', onCloseBtn);
 };
 
-// window.addEventListener('keydown', onEscapeClose);
+window.addEventListener('keydown', onEscapeClose);
 export function onEscapeClose(event) {
+    
     if (event.code === 'Escape') {
-        return onCloseBtn()
+        // return onCloseBtn()
+        refs.modalWindow.classList.add('is-hidden'); 
     };
-    window.remuveEventListener('keydown', onEscapeClose);
+    
+    
+    // window.removeEventListener('keydown', onEscapeClose);
 };
