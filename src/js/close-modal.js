d3641476c5febc8b4101131b8f9be8de5be43215
Doroshuk <dorosukolga100@gmail.com>
2021-07-25 23:44:21 +0300

    close button variant

--- src/js/close-modal.js
+++ src/js/close-modal.js
@@ -1,25 +1,46 @@
+
+
 const refs = {
     closeBtn: document.querySelector('[data-modal-close]'),
   modalWindow: document.querySelector('.backdrop'),
-  link: document.querySelector('.footer_link'),
+  
 }
-
-refs.link.addEventListener('click', onCloseBtn)
-
 refs.closeBtn.addEventListener('click', onCloseBtn);
 export function onCloseBtn(event) {
-    refs.modalWindow.classList.remove('is-hidden');
-    // refs.closeBtn.removeEventListener('click', onCloseBtn);
+    refs.modalWindow.classList.remove('is-open');
+    refs.closeBtn.removeEventListener('click', onCloseBtn);
 };
 
 window.addEventListener('keydown', onEscapeClose);
 export function onEscapeClose(event) {
-    
     if (event.code === 'Escape') {
-        // return onCloseBtn()
-        refs.modalWindow.classList.add('is-hidden'); 
+        onCloseBtn()
     };
-    
-    
-    // window.removeEventListener('keydown', onEscapeClose);
+    window.removeEventListener('keydown', onEscapeClose);
 };
+
+// const refs = {
+//     closeBtn: document.querySelector('[data-modal-close]'),
+//   modalWindow: document.querySelector('.backdrop'),
+//   link: document.querySelector('.footer_link'),
+// }
+
+// refs.link.addEventListener('click', onCloseBtn)
+
+// refs.closeBtn.addEventListener('click', onCloseBtn);
+// export function onCloseBtn(event) {
+//     refs.modalWindow.classList.remove('is-hidden');
+//     refs.closeBtn.removeEventListener('click', onCloseBtn);
+// };
+
+// window.addEventListener('keydown', onEscapeClose);
+// export function onEscapeClose(event) {
+    
+//     if (event.code === 'Escape') {
+//         // return on.CloseBtn()
+//         refs.modalWindow.classList.add('is-hidden'); 
+//     };
+    
+    
+//     // window.removeEventListener('keydown', onEscapeClose);
+// };
