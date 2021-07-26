0e5ba0c20900d4a9397991c3d0d79a0bfcdaea8e
Doroshuk <dorosukolga100@gmail.com>
2021-07-26 15:27:34 +0300

    Update close-modal.js

--- src/js/close-modal.js
+++ src/js/close-modal.js
@@ -3,28 +3,33 @@
 
 const refs = {
     closeBtn: document.querySelector('[data-modal-close]'),
-  modalWindow: document.querySelector('.backdrop'),
-    link: document.querySelector('.footer_link'),
-  modal: document.querySelector('.footer-modal'),
+  modalWindow: document.querySelector('[data-modal]'),
+    link: document.querySelector('[data-modal-open]'),
+//   modal: document.querySelector('.footer-modal'),
 }
 
 refs.link.addEventListener('click', onCloseBtn)
+
+
 refs.closeBtn.addEventListener('click', () => {
-   refs.modalWindow.classList.add('is-hidden'); 
-})
+    refs.modalWindow.classList.add('is-hidden');
+});
+
+
 refs.closeBtn.addEventListener('click', onCloseBtn);
-export function onCloseBtn(event) {
+
+export function onCloseBtn() {
     refs.modalWindow.classList.remove('is-hidden');
     refs.closeBtn.removeEventListener('click', onCloseBtn);
 };
 
 document.addEventListener('keydown', onEscapeClose);
+
 export function onEscapeClose(event) {
-    
 
     if (event.code === 'Escape') {
     
-        refs.modalWindow.classList.add('is-hidden');
+     refs.modalWindow.classList.add('is-hidden');
     } 
     
 }
