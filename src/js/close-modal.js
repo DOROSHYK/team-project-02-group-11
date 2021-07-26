c3c33d47565e4d6acedea06cdb48c610ae8aeb16
Doroshuk <dorosukolga100@gmail.com>
2021-07-26 15:22:47 +0300

    пофіксила відкриття модалки

--- src/js/close-modal.js
+++ src/js/close-modal.js
@@ -1,48 +1,43 @@
 
-// const refs = {
-//     closeBtn: document.querySelector('[data-modal-close]'),
-//   modalWindow: document.querySelector('.backdrop'),
-  
-// }
-
-// refs.closeBtn.addEventListener('click', onCloseBtn);
-// export function onCloseBtn(event) {
-//     refs.modalWindow.classList.remove('is-hidden');
-//     refs.closeBtn.removeEventListener('click', onCloseBtn);
-// };
-
-// window.addEventListener('keydown', onEscapeClose);
-// export function onEscapeClose(event) {
-//     if (event.code === 'Escape') {
-//         onCloseBtn()
-//     };
-//     window.removeEventListener('keydown', onEscapeClose);
-// };
 
 
 const refs = {
     closeBtn: document.querySelector('[data-modal-close]'),
   modalWindow: document.querySelector('.backdrop'),
-  link: document.querySelector('.footer_link'),
+    link: document.querySelector('.footer_link'),
+  modal: document.querySelector('.footer-modal'),
 }
 
 refs.link.addEventListener('click', onCloseBtn)
-
+refs.closeBtn.addEventListener('click', () => {
+   refs.modalWindow.classList.add('is-hidden'); 
+})
 refs.closeBtn.addEventListener('click', onCloseBtn);
 export function onCloseBtn(event) {
     refs.modalWindow.classList.remove('is-hidden');
     refs.closeBtn.removeEventListener('click', onCloseBtn);
 };
 
-window.addEventListener('keydown', onEscapeClose);
+document.addEventListener('keydown', onEscapeClose);
 export function onEscapeClose(event) {
     
+
     if (event.code === 'Escape') {
-        // return on.CloseBtn()
-        refs.modalWindow.classList.add('is-hidden'); 
-    };
     
+        refs.modalWindow.classList.add('is-hidden');
+    } 
     
-    // window.removeEventListener('keydown', onEscapeClose);
-};
+}
+
+window.addEventListener('click', (event) => {
+    if (event.target === refs.modalWindow) {
+       refs.modalWindow.classList.add('is-hidden');  
+    } 
+})
+
+
+
+
+
+
 
