b82dbab278786ba610728c50bdeb77640e373c2c
kostuk42 <kostuk42@i.ua>
2021-07-27 11:50:02 +0300

    adds method 'getObjectForRender' to serverAPI

--- src/js/filmCardShot.js
+++ src/js/filmCardShot.js
@@ -1,22 +1,11 @@
 import cardTemplate from '../template/filmCardShot.hbs';
 import ServerAPI from './serverAPI';
 const api = new ServerAPI;
-import genres from './genres.json'
-console.log(genres[0].id);
 
-api.getFilmByKeyword('sherlock').then(data => {
-    const genres = api.getGenreById(data.results[0].genre_ids)
-    console.log(data.results[0]);
-    console.log(genres);
-    const filmData = data.results[0]
-    const options = {
-        poster_path: filmData.poster_path,
-        title: filmData.title,
-        genres: genres.join(', '),
-        year: filmData.release_date.slice(0, 4),
-        vote_average: filmData.vote_average
-    }
-    const markup = cardTemplate(options);
+api.getFilmByKeyword('throne').then(data => {
+    const filmData = data.results[0];
+    console.log(api.getObjectForRender(filmData));
+    const markup = cardTemplate(api.getObjectForRender(filmData));
     console.log(markup)
     // document.body.insertAdjacentHTML('afterbegin', markup);
     // document.body.insertAdjacentHTML('afterbegin', markup);
