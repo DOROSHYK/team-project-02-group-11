9470b09d93f23785f257c0368ad81eed0c56545a
Doroshuk <dorosukolga100@gmail.com>
2021-07-29 22:45:39 +0300

    add spin

--- src/js/create_card.js
+++ src/js/create_card.js
@@ -1,21 +1,26 @@
 
 import cardImage from '../template/filmCardShot';
 
-import ServerAPI from './serverAPI';
+import ServerAPI from './serverAPI.js';
 import getRefs from './get-refs';
+import { startSpin, stopSpin } from './spiner/spiner.js';
 
 const refs = getRefs();
 const API = new ServerAPI;
 
 export default function createCard(someWords) {
+  
 
     if (someWords.length !== 0) {
+
+        
         const resultImages = someWords.results.map(result => {
             return cardImage(result);
         })
 
         refs.popFilmList.classList.add('visually-hidden');
         refs.gallery.insertAdjacentHTML('beforeend', resultImages);
+        
             
     }
     
