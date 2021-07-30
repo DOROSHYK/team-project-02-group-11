f068b2f514cf93f15ddff6e58f46b662879f2515
kostuk42 <kostuk42@i.ua>
2021-07-30 19:02:48 +0300

    commented excess

--- src/js/filmCardShot.js
+++ src/js/filmCardShot.js
@@ -11,24 +11,24 @@ api.getFilmByKeyword('throne').then(data => {
     console.log(api.getObjectForRender(filmData));
     const markup = cardTemplate(api.getObjectForRender(filmData));
     console.log(markup)
-    document.body.insertAdjacentHTML('afterbegin', markup);
+    // document.body.insertAdjacentHTML('afterbegin', markup);
     // document.body.insertAdjacentHTML('afterbegin', markup);
 })
-const onClick = (e) => {
+// const onClick = (e) => {
     
-    e.preventDefault();
-    console.log(e.target.className);
-    const id = e.path[1].dataset.id || e.path[2].dataset.id;
-    if (!id) return 
-    api.getFilmInfoById(+id).then(dataFilm => {
-        const markObj = api.getObjectForRender(dataFilm);
-        LS.addFilm(markObj);
-        // LS.setQueue();
-        const markup = filmcardModal(markObj);
-        document.body.insertAdjacentHTML('afterbegin', markup);
-    })
-}
-document.body.addEventListener('click', onClick);
+//     e.preventDefault();
+//     console.log(e.target.className);
+//     const id = e.path[1].dataset.id || e.path[2].dataset.id;
+//     if (!id) return 
+//     api.getFilmInfoById(+id).then(dataFilm => {
+//         const markObj = api.getObjectForRender(dataFilm);
+//         LS.addFilm(markObj);
+//         // LS.setQueue();
+//         const markup = filmcardModal(markObj);
+//         document.body.insertAdjacentHTML('afterbegin', markup);
+//     })
+// }
+// document.body.addEventListener('click', onClick);
 
 
 
