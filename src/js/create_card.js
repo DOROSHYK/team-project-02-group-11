e742d3e902c9188f46639b42686c876039fbf728
OLgaPonomarenko <fblackberries@gmail.com>
2021-07-31 15:18:03 +0300

    genre
    genre, debounce

--- src/js/create_card.js
+++ src/js/create_card.js
@@ -1,22 +1,31 @@
 
 import cardImage from '../template/filmCardShot';
-
 import ServerAPI from './serverAPI';
 import getRefs from './get-refs';
 
 const refs = getRefs();
 const API = new ServerAPI;
 
+let resultImages;
+
 export default function createCard(someWords) {
 
     if (someWords.length !== 0) {
-        const resultImages = someWords.results.map(result => {
-            return cardImage(result);
-        })
 
+        resultImages = someWords.results.map(result => {
+             
+            let some = API.getObjectForRender(result); // или одной  строкой?
+            return   cardImage(some);
+        
+       } )
+    } 
+    
         refs.popFilmList.classList.add('visually-hidden');
         refs.gallery.insertAdjacentHTML('beforeend', resultImages);
             
-    }
-    
 }
+    
+
+
+
+
