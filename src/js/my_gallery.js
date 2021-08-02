1f4dd976a95e53c84e951271c375661fa32ad2de
OLgaPonomarenko <fblackberries@gmail.com>
2021-08-02 22:51:21 +0300

    my clear

--- src/js/my_gallery.js
+++ src/js/my_gallery.js
@@ -1,40 +1,17 @@
 import getRefs from './get-refs';
 import Local from './LocalStorage';
-import ServerAPI from './serverAPI';
 import cardImage from '../template/filmCardShot';
 
-
 const loc = new Local();
-const refs = getRefs();
-const API = new ServerAPI;
-let currentID = null;
+let refs = getRefs();
+let someDate = loc.getWatched();
 
-refs.mainRef.addEventListener('click', onChangeMyLibrary);
 refs.headerBtnWrap.addEventListener('click', onMyLibrary);
 
-function onChangeMyLibrary(e) {
-  const refs = getRefs();
-  const currentEl = e.target.parentNode;
-  const currentID = currentEl.parentNode.dataset.id;
-
-  API.getFilmInfoById(currentID).then(saveMe);
-
-  function saveMe(some) {
-    
-    const newFilm = API.getObjectForRender(some);
-    loc.addFilm(newFilm);
-
-    if (e.target.classList.contains('add-to-watched')) loc.setWatched();
-    else if (e.target.classList.contains('add-to-queue')) loc.setQueue();
-
-  }
-};
-
 function onMyLibrary(e) {
-  const refs = getRefs();
+  refs = getRefs();
   refs.clientGallery.innerHTML = '';
-  let someDate = null;
-
+  
   if (e.target.textContent === 'Watched') {
       someDate = loc.getWatched();
   }
