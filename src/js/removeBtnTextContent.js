a309d16e66773c8d9bdf5b3aa0cf30da735c758d
Вікторія Щербанич <dyaral696@gmail.com>
2021-08-04 18:52:57 +0300

    Update removeBtnTextContent.js

--- src/js/removeBtnTextContent.js
+++ src/js/removeBtnTextContent.js
@@ -5,21 +5,24 @@ export default function onClickAddOrRemoveBtn(e) {
   
     if (e.target.textContent === 'Add to watched') {
         e.target.classList.add('remove-from-watched');
+        e.target.classList.remove('add-to-watched');
         e.target.textContent = 'Remove watched';
     }
 
     else if (e.target.textContent === 'Add to queue') {
         e.target.classList.add('remove-from-queue');
+        e.target.classList.remove('add-to-queue');
         e.target.textContent = 'Remove queue';
     }
 
     else if (e.target.textContent === 'Remove queue') {
-        console.log(1)
+        e.target.classList.add('add-to-queue');
         e.target.classList.remove('remove-from-queue');
         e.target.textContent = 'Add to queue';
     }
 
     else if (e.target.textContent === 'Remove watched') {
+        e.target.classList.add('add-to-watched');
         e.target.classList.remove('remove-from-watched');
         e.target.textContent = 'Add to watched';
     }
