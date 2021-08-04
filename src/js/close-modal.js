869ff1839522d4e58f078531b18330a4fff77570
Boiko Roman <boykoroman94@gmail.com>
2021-08-04 14:13:16 +0300

    пофиксил открытие и закрытие модалок, подправил стили

--- src/js/close-modal.js
+++ src/js/close-modal.js
@@ -1,67 +1,92 @@
 import getRefs from './get-refs';
-const refsMod = getRefs();
-
-const refs = {
-        closeBtn: document.querySelector('[data-modal-close]'),
-    modalWindow: document.querySelector('[data-modal]'),
-        link: document.querySelector('[data-modal-open]'),
-    //   modal: document.querySelector('.footer-modal'),
-};
+const refs = getRefs();
 
 
-// FOR FOOTER MODAL
-refsMod.footerModalOpener.addEventListener('click',onOpenTeamModal);
+refs.footerModalOpener.addEventListener('click', onOpenTeamModal);
 
 function onOpenTeamModal(){
-    refsMod.footerModal.classList.remove('is-hidden');
-    refsMod.footerModalOpener.removeEventListener('click',onOpenTeamModal);
+    refs.footerModal.classList.remove('is-hidden');
+    document.body.style.overflow = 'hidden';
+    refs.footerModal.addEventListener('click', closeFooterModal);
+    document.addEventListener('keydown', onEscapeClose);
 };
-
-refsMod.footerCloseModalBtn.addEventListener('click', onCloseFooterBtn);
-
-function onCloseFooterBtn(){
-    refsMod.footerModal.classList.add('is-hidden');
-    refsMod.footerCloseModalBtn.removeEventListener('click', onCloseFooterBtn);
-};
-// FOR FOOTER MODAL
-
-// refs.link.addEventListener('click', onCloseBtn)
-
-
-
-refs.closeBtn.addEventListener('click', () => {
-    refs.modalWindow.classList.add('is-hidden');
-    document.body.style.overflow = 'auto';
-});
-
-
-refs.closeBtn.addEventListener('click', onCloseBtn);
-
-export function onCloseBtn() {
-    refs.modalWindow.classList.remove('is-hidden');
-    refs.closeBtn.removeEventListener('click', onCloseBtn);
-};
-
-document.addEventListener('keydown', onEscapeClose);
 
 export function onEscapeClose(event) {
-
     if (event.code === 'Escape') {
-    
-        refs.modalWindow.classList.add('is-hidden');
+        hideModal();
         document.body.style.overflow = 'auto';
     } 
-    
 }
 
-window.addEventListener('click', (event) => {
-    if (event.target === refs.modalWindow) {
-        refs.modalWindow.classList.add('is-hidden');
-        document.body.style.overflow = 'auto';
-    } 
-})
+function closeFooterModal(event) {
+    onCloseFooterBtnClick(event);
+    onFooterBackdropClick(event);
+}
+
+function onCloseFooterBtnClick(event) {
+    if (event.target.classList.contains('close-footer-modal-js') || event.target.classList.contains('js-close-footer-modal-icon')) {
+        hideModal();
+        removeListeners();
+    }    
+};
 
 
+function onFooterBackdropClick(event) {
+    if (!event.target.classList.contains('js-footer-backdrop')) {
+        return;
+    }
+    hideModal();
+    removeListeners();
+}
+
+function removeListeners() {
+    document.removeEventListener('keydown', onEscapeClose);
+    refs.footerModal.removeEventListener('click', closeFooterModal);
+    document.body.style.overflow = 'auto';
+}
+
+function hideModal() {
+    refs.footerModal.classList.add('is-hidden');
+    refs.movieModal.classList.add('is-hidden');
+}
+
+//movieModal
+
+export function addMovieModalListener() {
+    refs.movieModal.addEventListener('click', closeMovieModal);
+    document.body.style.overflow = 'hidden';
+    document.addEventListener('keydown', onEscapeClose);
+}
+
+function closeMovieModal(event) {
+    onCloseMovieBtnClick(event);
+    onMovieBackdropClick(event);
+}
+
+function onCloseMovieBtnClick(event) {
+    if (event.target.classList.contains('close-movie-modal-js') || event.target.classList.contains('js-close-movie-modal-icon')) {
+        hideMovieModal();
+        removeMovieListeners();
+    }
+}
+
+function hideMovieModal() {
+    refs.movieModal.classList.add('is-hidden');
+}
+
+function removeMovieListeners() {
+    document.removeEventListener('keydown', onEscapeClose);
+    refs.movieModal.removeEventListener('click', closeMovieModal);
+    document.body.style.overflow = 'auto';
+}
+
+function onMovieBackdropClick(event) {
+    if (!event.target.classList.contains('js-movie-backdrop')) {
+        return;
+    }
+    hideMovieModal();
+    removeMovieListeners();
+}
 
 
 
