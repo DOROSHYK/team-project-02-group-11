0de5fa011f74344083cd888d714f3f9f7792f547
OLgaPonomarenko <fblackberries@gmail.com>
2021-08-05 17:07:22 +0300

    4

--- src/js/switch_theme.js
+++ src/js/switch_theme.js
@@ -11,8 +11,13 @@ refs.buttonTheme.addEventListener('click', switchTheme);
 
 
 function switchTheme(e) {   
+<<<<<<< Updated upstream
   
      if (!e.target.checked){
+=======
+    refs = getRefs();
+     if (e.target.checked){
+>>>>>>> Stashed changes
         refs.allPage.classList.add(Theme.LIGHT);
         refs.allPage.classList.remove(Theme.DARK);
         localStorage.setItem('Theme', Theme.LIGHT);
@@ -42,17 +47,53 @@ function switchTheme(e) {
           
          refs.allPage.classList.add(Theme.DARK);
        refs.allPage.classList.remove(Theme.LIGHT);
-         localStorage.setItem('Theme', Theme.DARK);
+       localStorage.setItem('Theme', Theme.DARK);
+       
+        refs.iconTheme.animate(
+        [
+          // keyframes
+              { transform: 'translateY(0px)' },
+              { transform: 'translateY(25px)' },
+              { transform: 'translateY(50px)' },
+              { transform: 'translateY(100px)' }
+        ], {
+              // timing options
+              duration: 1000
+            }
+       );
+       refs.iconHideTheme.animate(
+        [
+              { transform: 'translateY(0px)' }
+        ], {
+              // timing options
+              duration: 1000
+            }
+       ) 
     }
+<<<<<<< Updated upstream
   savedTheme(); 
+=======
+   savedTheme(); 
+>>>>>>> Stashed changes
 };
 
 function savedTheme() {
   const saveTheme = localStorage.getItem('Theme');
   if (saveTheme) {
      refs.allPage.classList.add(saveTheme);
+<<<<<<< Updated upstream
      if (saveTheme === Theme.DARK) {
        refs.buttonTheme.checked = true;
      }
+=======
+      if (saveTheme === Theme.DARK) {
+       refs.buttonTheme.checked = true;
+      
+    }
+    
+
+>>>>>>> Stashed changes
   }
+
+  savedTheme(); 
 }
