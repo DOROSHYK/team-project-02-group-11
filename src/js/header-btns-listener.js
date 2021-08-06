418c6d84f13da6d0c49cb8eec1a5f34a51c1d49b
Вікторія Щербанич <dyaral696@gmail.com>
2021-08-06 23:35:30 +0300

    add disabled attribute

--- src/js/header-btns-listener.js
+++ src/js/header-btns-listener.js
@@ -1,6 +1,5 @@
 import getRefs from './get-refs';
-import Local from './LocalStorage';
-import cardImage from '../template/filmCardShot';
+import Local from './LocalStorage'; 
 import renderLibraryPage from './changePage';
 
 const loc = new Local();
@@ -19,7 +18,7 @@ function onMyLibrary(e) {
       }
       someDate = loc.getWatched();
       
-  }   
+    }   
     else if (e.target.textContent === 'Queue') {
         refs.headerBtns[1].classList.add('button--current');
         refs.headerBtns[0].classList.remove('button--current');
