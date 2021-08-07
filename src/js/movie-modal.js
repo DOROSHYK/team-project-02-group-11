257810cd263aec683bbbd75604bbfc57980fef4e
Вікторія Щербанич <dyaral696@gmail.com>
2021-08-07 17:55:25 +0300

    home page

--- src/js/movie-modal.js
+++ src/js/movie-modal.js
@@ -3,6 +3,7 @@ import movieCardTemplate from '../template/filmcardModal.hbs';
 import Api from './serverAPI';
 import LocalStorage from './LocalStorage';
 import { addMovieModalListener } from './close-modal';
+import renderLibraryPage from './changePage';
 const refs = getRefs();
 const api = new Api();
 const LS = new LocalStorage;
@@ -10,8 +11,8 @@ document.getElementById('app').addEventListener('click', onMovieClick);
 
 function onMovieClick(event) {
     event.preventDefault();
+    console.log(event.target)
     
-
     if (event.target.dataset.id || event.target.parentNode.dataset.id) {
         refs.movieModal.classList.remove('is-hidden');
         addMovieModalListener();
@@ -38,6 +39,10 @@ function onMovieClick(event) {
         document.body.style.overflow = 'hidden';
         
     }
+
+    setTimeout(() => {
+        checkFilmInLocalStorage(event.target.dataset.id);
+    }, 0);
 };
 
     getRefs().movieModal.addEventListener('click', onModalButtonClick);
@@ -47,15 +52,38 @@ export default function onModalButtonClick(e) {
     if (e.target.dataset.id || e.target.parentNode.dataset.id) {
         if (e.target.classList.contains('add-button')) {
 
-            if (e.target.textContent === 'Add to watched' || e.target.textContent === 'Remove watched') {
+            if (e.target.textContent === 'Add to watched') {
 
                 e.target.disabled = false;
                 e.target.nextElementSibling.disabled = true;
+                e.target.textContent = 'Remove watched';
+                e.target.className = 'add-to-watched watched add-button uppercase';
+                //e.target.className = 'remove-from-watched add-button uppercase';
             }
 
-            else if (e.target.textContent === 'Add to queue' || e.target.textContent === 'Remove queue') {
+            else if (e.target.textContent === 'Remove watched') {
+
                 e.target.disabled = false;
-                e.target.previousElementSibling.disabled = true;   
+                e.target.nextElementSibling.disabled = false;
+                e.target.textContent = 'Add to watched';
+                //e.target.className = 'add-to-watched watched add-button uppercase';
+                e.target.className = 'remove-from-watched add-button uppercase';
+            }
+
+            else if (e.target.textContent === 'Add to queue') {
+                e.target.disabled = false;
+                e.target.previousElementSibling.disabled = true;
+                e.target.textContent = 'Remove queue';
+                //e.target.className = 'remove-from-queue add-button uppercase';
+                e.target.className = 'add-button add-to-queue queue uppercase';
+            }
+
+            else if (e.target.textContent === 'Remove queue') {
+                e.target.disabled = false;
+                e.target.previousElementSibling.disabled = false;
+                e.target.textContent = 'Add to queue';
+                //e.target.className = 'add-button add-to-queue queue uppercase';
+                e.target.className = 'remove-from-queue add-button uppercase';
             };
         };
     };
@@ -87,3 +115,25 @@ function currentButton() {
     
 }
 
+ 
+  function checkFilmInLocalStorage(id) {
+        const refs = getRefs();
+        let filmsInWatched = [];
+        LS.getWatched().map (film => filmsInWatched.push(film.id));
+
+        let filmsInQueue = [];
+        LS.getQueue().map(film => filmsInQueue.push(film.id));
+        let idFilm = parseInt(id);
+
+        if (filmsInQueue.includes(idFilm)) {
+            console.log(1)
+            // console.log(getRefs().addToQueueBtnModal)
+            // getRefs().addToWatchedBtnModal.disabled = true;
+            // getRefs().addToQueueBtnModal.textContent = 'Remove queue';
+            // getRefs().addToQueueBtnModal.classList.add('remove-from-queue add-button uppercase');
+            // getRefs().addToQueueBtnModal.classList.remove('add-button add-to-queue queue uppercase');
+        }
+        else if (filmsInWatched.includes(idFilm)) {
+            console.log(2)
+         }
+};
